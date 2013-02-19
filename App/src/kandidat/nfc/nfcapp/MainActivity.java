d7c710835680d31073ca8016fe41c6fc7c077795
freein <freein@student.chalmers.se>
2013-02-19 15:21:43 +0100

    Implemented comunication protocol. Adding new class NFCPMessage.

--- App/src/kandidat/nfc/nfcapp/MainActivity.java
+++ App/src/kandidat/nfc/nfcapp/MainActivity.java
@@ -1,30 +1,32 @@
 package kandidat.nfc.nfcapp;
 
+import java.nio.charset.Charset;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.NfcAdapter.CreateNdefMessageCallback;
 import android.nfc.NfcEvent;
-import android.nfc.NfcManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.provider.Settings;
+import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.view.*;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
 public class MainActivity extends Activity  implements CreateNdefMessageCallback {
-	
+
+	//ndefmessage strings
+	private String latestRecievedMsg;
+	private NFCPMessage nfcpMessage;
 
 	//Objekt som representerar NFC adaptern
 	private NfcAdapter nfcAdapter;
 
-    @Override
+    @SuppressLint("NewApi")
+	@Override
     /**
      * This function is lanched when the app is started.
      */
@@ -36,16 +38,20 @@ public class MainActivity extends Activity  implements CreateNdefMessageCallback
         //Får tag i ett objekt som representerar NFC-adaptern
         nfcAdapter = NfcAdapter.getDefaultAdapter(this);
         
+        //Kolla om användarens NFC är påslagen
         if(!nfcAdapter.isEnabled()){
         	//Öppnar menyn så att användaren kan aktivera NFC
         	startActivity(new Intent(Settings.ACTION_NFC_SETTINGS));
-        } else if (!nfcAdapter.isNdefPushEnabled()) {
-            startActivity(new Intent(Settings.ACTION_NFCSHARING_SETTINGS));
         }
+        //Följande eftersom metoden endast finns för API 16 och högre medan t.ex. LG L5 har version 15
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN){
+            if (!nfcAdapter.isNdefPushEnabled()) {
+                startActivity(new Intent(Settings.ACTION_NFCSHARING_SETTINGS));
+            }
+        }
+
         
         nfcAdapter.setNdefPushMessageCallback(this, this);
-
-
     }
     
     /** Called when the user clicks the Send button */
@@ -54,17 +60,8 @@ public class MainActivity extends Activity  implements CreateNdefMessageCallback
     	//START LOADING SCREEN FIRST????
     	//SEND AND RECIEVE NFC MESSAGE HERE IF WE GET WHAT WE WANT ->>
     	//START ACCESSACTIVITY ELSE START DENIEDACTIVITY
-    	
     	//NFC ACCESS
-    	if(true){
-    		Intent intent = new Intent(this, AccessActivity.class);
-    		startActivity(intent);
-    	}
-    	else{
-    		//NOT NFC ACCESS
-    		Intent intent = new Intent(this, DeniedActivity.class);
-    	    startActivity(intent);
-    	}
+    	
     }
    
     
@@ -87,14 +84,14 @@ public class MainActivity extends Activity  implements CreateNdefMessageCallback
 
         super.onPause();
 
-        //mAdapter.disableForegroundDispatch(this);
+        //nfcAdapter.disableForegroundDispatch(this);
 
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        //mAdapter.enableForegroundDispatch(this, pendingIntent, 
+        //nfcAdapter.enableForegroundDispatch(this, pendingIntent, 
          //   intentFiltersArray, techListsArray);
         
         // Check to see that the Activity started due to an Android Beam
@@ -104,31 +101,81 @@ public class MainActivity extends Activity  implements CreateNdefMessageCallback
 
     }
 
-   
-    void processIntent(Intent intent) {
-        Parcelable[] rawMsgs = intent.getParcelableArrayExtra(
-                NfcAdapter.EXTRA_NDEF_MESSAGES);
-        // only one message sent during the beam
-        NdefMessage msg = (NdefMessage) rawMsgs[0];
-        // record 0 contains the MIME type, record 1 is the AAR, if present
-        Toast.makeText(this, new String(msg.getRecords()[0].getPayload()), Toast.LENGTH_LONG).show();
-    }
+   /**
+    * Receives intent and save and show message
+    * @param intent
+    */
+	void processIntent(Intent intent) {
+
+		Parcelable[] rawMsgs = intent
+				.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+		// only one message sent during the beam
+		NdefMessage msg = (NdefMessage) rawMsgs[0];
+		latestRecievedMsg = new String(msg.getRecords()[0].getPayload());
+		// record 0 contains the MIME type, record 1 is the AAR, if present
+		// Toast.makeText(this, latestRecievedMsg, Toast.LENGTH_LONG).show();
+		TextView view = (TextView) findViewById(R.id.message);
+		view.setText(latestRecievedMsg);
+		
+		nfcpMessage = new NFCPMessage(latestRecievedMsg);
+		
+		if(nfcpMessage.getStatus() && nfcpMessage.getType()==3){
+			startActivity(new Intent(this, AccessActivity.class));
+    	}
+    	else if(!nfcpMessage.getStatus() && nfcpMessage.getType()==3){
+    		//NOT NFC ACCESS
+    		Intent deniedIntent =new Intent(this, DeniedActivity.class);
+    		deniedIntent.putExtra("ErrorCode", nfcpMessage.getErrorCode());
+    	    startActivity(deniedIntent);
+    	}
+		
+	}
     
     @Override
     public void onNewIntent(Intent intent) {
         // onResume gets called after this to handle the intent
-        setIntent(intent);
+         setIntent(intent);
     }
-    @Override
-    public NdefMessage createNdefMessage(NfcEvent event) {
-        String text = ("Beam me up, Android!\n\n" +
-                "Beam Time: " + System.currentTimeMillis());
-        NdefMessage msg = new NdefMessage(
-                new NdefRecord[] { 
-                		NdefRecord.createMime(
-                        "application/vnd.com.example.android.beam", text.getBytes())
-        });
-        return msg;
-    } 
-}
+    
+	@SuppressLint("NewApi")
+	@Override
+	public NdefMessage createNdefMessage(NfcEvent event) {
+		NFCPMessage sendMsg = null;
 
+		if (nfcpMessage == null) {
+			// return null;
+
+			// testing
+			sendMsg = new NFCPMessage("NFC", 1, true, 1, 0);
+
+		} else if (nfcpMessage.getType() == 1) {
+
+			// skicka typ 2 meddelande
+
+			sendMsg = new NFCPMessage("NFC", 1, 2, 1234);
+
+		} else if (nfcpMessage.getType() == 2) {
+			// testkod för att ta emot meddelande typ 2
+
+			// skicka meddelande typ 3
+			sendMsg = new NFCPMessage("NFC", 1, true, 3, 0);
+
+		} else if (nfcpMessage.getType() == 3) {
+			// att göra när vi tar emot meddelande typ 3
+			return null;
+
+		} else {
+			// allt går fel
+		}
+
+		NdefMessage msg;
+
+		NdefRecord record = new NdefRecord(NdefRecord.TNF_MIME_MEDIA,
+				"text/plain".getBytes(Charset.forName("US-ASCII")),
+				new byte[0], sendMsg.toString().getBytes(
+						Charset.forName("US-ASCII")));
+		msg = new NdefMessage(new NdefRecord[] { record });
+		return msg;
+	}
+	
+}
---
+++ App/src/kandidat/nfc/nfcapp/NFCPMessage.java
@@ -0,0 +1,80 @@
+package kandidat.nfc.nfcapp;
+
+import java.util.Scanner;
+
+public class NFCPMessage {
+
+	private String name ="";
+	private int id =0;
+	private int typeOfMessage=0;
+	private int key=0;
+	private boolean status=false;
+	private int errorCode=0;
+	
+	
+	//Constructor Type1 and Type3 Message
+	public NFCPMessage(String name, int id, boolean status, int typeOfMessage,
+			int errorCode) {
+		super();
+		this.name = name;
+		this.id = id;
+		this.status = status;
+		this.typeOfMessage= typeOfMessage;
+		this.errorCode = errorCode;
+	}
+	
+	//Constructor Type2 Message
+	public NFCPMessage(String name, int id, int typeOfMessage, int key) {
+		super();
+		this.name = name;
+		this.id = id;
+		this.typeOfMessage = typeOfMessage;
+		this.key = key;
+	}
+	
+	//Constructor for received Strings
+	public NFCPMessage(String s){
+		Scanner sc = new Scanner(s);
+			name = sc.nextLine();
+			sc.next();
+			id = sc.nextInt();
+			sc.next();
+			typeOfMessage = sc.nextInt();
+			sc.next();
+			key = sc.nextInt();
+			sc.next();
+			status = sc.nextBoolean();
+			sc.next();
+			errorCode = sc.nextInt();
+		
+	}
+	
+	
+	
+	@Override
+	public String toString(){
+		
+		String protocolTemplate =name+ "\n" +
+				"Låsid: "+id+"\n" +
+				"Typ: "+typeOfMessage+ "\n" +
+				"Nyckel: "+key + "\n" +
+				"Status: "+status+ "\n" +
+				"Felkod: "+errorCode;
+		
+		return protocolTemplate;
+		
+	}
+
+	public int getType(){
+		return typeOfMessage;
+}
+
+	public boolean getStatus() {
+		return status;
+	}
+
+	public int getErrorCode() {
+		return errorCode;
+	}
+	
+}
