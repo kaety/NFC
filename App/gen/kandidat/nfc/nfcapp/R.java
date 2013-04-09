98c6ad9d67492137379b9da35e088fa8ef8e2a57
robijoha <robijoha@CE-PC20.net.chalmers.se>
2013-04-09 18:43:28 +0200

    First Push

--- App/gen/kandidat/nfc/nfcapp/R.java
+++ App/gen/kandidat/nfc/nfcapp/R.java
@@ -22,50 +22,63 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int ic_menu_btn_add=0x7f020001;
+        public static final int ic_menu_delete=0x7f020002;
+        public static final int ic_menu_search_holo_light=0x7f020003;
+        public static final int ic_menu_share_holo_light=0x7f020004;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f080009;
-        public static final int action_settings=0x7f08001b;
+        public static final int LinearLayout1=0x7f08000a;
+        public static final int action_settings=0x7f08001e;
         public static final int button1=0x7f080004;
-        public static final int button2=0x7f080006;
-        public static final int button3=0x7f080005;
-        public static final int button4=0x7f080008;
+        public static final int button2=0x7f080005;
+        public static final int button3=0x7f080007;
+        public static final int button4=0x7f080006;
+        public static final int door_id=0x7f08001c;
+        public static final int door_key=0x7f08001d;
         public static final int editText1=0x7f080002;
         public static final int editText2=0x7f080003;
         public static final int errorLabel=0x7f080001;
-        public static final int message=0x7f08000b;
-        public static final int oldpassword=0x7f08000f;
-        public static final int passwordConfirm=0x7f08000e;
-        public static final int passwordCreate=0x7f08000d;
-        public static final int passwordView=0x7f08000c;
-        public static final int radioButton1=0x7f080012;
-        public static final int ratingBar1=0x7f080013;
-        public static final int ratingBar2=0x7f080014;
-        public static final int ratingBar3=0x7f080015;
-        public static final int ratingBar4=0x7f080016;
-        public static final int ratingBar5=0x7f080017;
-        public static final int ratingBar6=0x7f080018;
-        public static final int ratingBar7=0x7f080019;
-        public static final int ratingBar8=0x7f08001a;
-        public static final int scrollView1=0x7f080011;
-        public static final int submitPassword=0x7f080010;
-        public static final int textView1=0x7f080007;
+        public static final int item1=0x7f08001f;
+        public static final int item2=0x7f080020;
+        public static final int message=0x7f08000c;
+        public static final int oldpassword=0x7f080010;
+        public static final int passwordConfirm=0x7f08000f;
+        public static final int passwordCreate=0x7f08000e;
+        public static final int passwordView=0x7f08000d;
+        public static final int radioButton1=0x7f080013;
+        public static final int ratingBar1=0x7f080014;
+        public static final int ratingBar2=0x7f080015;
+        public static final int ratingBar3=0x7f080016;
+        public static final int ratingBar4=0x7f080017;
+        public static final int ratingBar5=0x7f080018;
+        public static final int ratingBar6=0x7f080019;
+        public static final int ratingBar7=0x7f08001a;
+        public static final int ratingBar8=0x7f08001b;
+        public static final int scrollView1=0x7f080012;
+        public static final int submitPassword=0x7f080011;
+        public static final int textView1=0x7f080008;
         public static final int textView2=0x7f080000;
-        public static final int title=0x7f08000a;
+        public static final int title=0x7f08000b;
+        public static final int twoLineListItem1=0x7f080009;
     }
     public static final class layout {
         public static final int activity_access=0x7f030000;
         public static final int activity_denied=0x7f030001;
         public static final int activity_keys=0x7f030002;
-        public static final int activity_login=0x7f030003;
-        public static final int activity_main=0x7f030004;
-        public static final int activity_password=0x7f030005;
-        public static final int activity_settings=0x7f030006;
-        public static final int activity_share=0x7f030007;
+        public static final int activity_keys_second_edition=0x7f030003;
+        public static final int activity_login=0x7f030004;
+        public static final int activity_main=0x7f030005;
+        public static final int activity_password=0x7f030006;
+        public static final int activity_settings=0x7f030007;
+        public static final int activity_share=0x7f030008;
+        public static final int dialog_add=0x7f030009;
     }
     public static final class menu {
         public static final int keys=0x7f070000;
-        public static final int share=0x7f070001;
+        public static final int keys_activity=0x7f070001;
+        public static final int main_activity=0x7f070002;
+        public static final int share=0x7f070003;
     }
     public static final class string {
         public static final int access_denied=0x7f050006;
@@ -74,7 +87,10 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int button_send=0x7f050002;
         public static final int button_settings=0x7f050001;
+        public static final int door_id=0x7f05000d;
+        public static final int door_key=0x7f05000e;
         public static final int hello_world=0x7f05000b;
+        public static final int keys_create=0x7f05000f;
         public static final int title_activity_access=0x7f050003;
         public static final int title_activity_denied=0x7f050005;
         public static final int title_activity_keys=0x7f050009;
